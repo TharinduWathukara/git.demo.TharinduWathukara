d066cdadd849db5baf9fb99906f8380a2c7d738b
TharinduWathukara <wtgwathnkara.15@cse.mrt.ac.lk>
2017-02-02 09:38:53 +0530

    Added the java project files

---
+++ Main.java
@@ -0,0 +1,29 @@
+/**
+* Created by prabhathp on 1/23/17.
+*/
+public class Main {
+	public static void main(String[] args) {
+		int a;
+		int b;
+		String operation;
+		int answer;
+		Sca nner scanner = new Scanner(System. in);
+		System. out.println( "Welcome to my calculator" );
+		System. out.println( "Enter first number:" );
+		a = scanner.nextInt();
+		System. out.println( "Enter second number:" );
+		b = scanner.nextInt();
+		System. out.println( "Enter operation:" );
+		operation = scanner.next();
+		if ( "+" .equals(operation)) {
+			answer = add(a, b);
+			System. out.println( "Answer is: " + answer);
+		}
+		else {
+			System. out.println( "Unsupported operation" );
+			}
+		}
+		private static int add( int a, int b) {
+		return a + b;
+	}
+}
